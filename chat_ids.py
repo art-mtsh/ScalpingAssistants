540bf31c5eda5cea270c8fa82fe05df3ca2d3471
art-mtsh <108072766+art-mtsh@users.noreply.github.com>
2024-08-12 16:52:23 +0300

    First commit

--- chat_ids.py
+++ chat_ids.py
@@ -1,30 +1,30 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-# # Path to your service account key file
-# SERVICE_ACCOUNT_KEY = 'key.json'
-#
-# # Initialize the Firebase Admin SDK
-# cred = credentials.Certificate(SERVICE_ACCOUNT_KEY)
-# firebase_admin.initialize_app(cred, {
-#     'databaseURL': 'https://tgbots-8fc9c-default-rtdb.europe-west1.firebasedatabase.app/'
-# })
-#
-# # Reference to your Realtime Database
-# ref = db.reference('chat_ids')
-#
-# def get_existed_chat_ids():
-#     return ref.get() or []
-#
-# def save_new_chat_id(new_chat_id=None):
-#     existed_chat_ids = get_existed_chat_ids()
-#     if new_chat_id and new_chat_id not in existed_chat_ids:
-#         existed_chat_ids.append(new_chat_id)
-#     ref.set(existed_chat_ids)
+# Path to your service account key file
+SERVICE_ACCOUNT_KEY = 'key.json'
 
+# Initialize the Firebase Admin SDK
+cred = credentials.Certificate(SERVICE_ACCOUNT_KEY)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://tgbots-8fc9c-default-rtdb.europe-west1.firebasedatabase.app/'
+})
+
+# Reference to your Realtime Database
+ref = db.reference('chat_ids')
 
 def get_existed_chat_ids():
-    return [662482931]
+    return ref.get() or []
 
 def save_new_chat_id(new_chat_id=None):
-    return None
+    existed_chat_ids = get_existed_chat_ids()
+    if new_chat_id and new_chat_id not in existed_chat_ids:
+        existed_chat_ids.append(new_chat_id)
+    ref.set(existed_chat_ids)
+
+#
+# def get_existed_chat_ids():
+#     return [662482931]
+#
+# def save_new_chat_id(new_chat_id=None):
+#     return None
